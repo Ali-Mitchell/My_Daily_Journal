9cfd9204b4c1c4e3f7636f1239be17a20b7bad10
AliMitchell <ali.mitchell.n@gmail.com>
2021-04-18 15:39:52 -0600

    Trouble shooting

--- assets/calender.js
+++ assets/calender.js
@@ -1,7 +1,17 @@
-var amSave = document.getElementById("am-input")
+
+var addToForm = document.getElementById('am-input');
+var saveInput = document.getElementById('input');
+var saveBtn = document.getElementById('am-btn');
+
+savebtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    addToForm.textContent = saveInput.value;
+    localStorage.setItem('saveInput.value');
+});
 
 
-// var saveAM = function(event) {
+
+// var saveInput = function(event) {
 //     //Why do we have to do this?
 //     event.preventDefault();
 //     // only run if the submit button is being clicked
@@ -10,7 +20,7 @@ var amSave = document.getElementById("am-input")
 //         // get the initial entry form element
 //         var amInputEl = document.querySelector("#am-input");
 //         var amUserInput = amInputEl.value
-//         // ensure initials have been entered
+//         // ensure that text has been entered 
 //         if(!amUserInputEl) {
 //             alert("Please enter text to save to your journal");
 //             return false;
@@ -32,10 +42,8 @@ var amSave = document.getElementById("am-input")
 //     }
 // };
 
-// function to save to localStorage
-var saveEvents = function () {
-    localStorage.setItem("events", JSON.stringify(events));
-};
+
+
 
 // variable for events to be stored, pull from localStorage, set to empty array if falsy
 var events = JSON.parse(localStorage.getItem("events")) || [];
@@ -48,11 +56,11 @@ var loadEvents = function() {
 };
 
 
-// function to push description to array upon save button click
-$(".saveBtn").on("click", function () {
-    // get the eventID of the button clicked
-    var eventId = $(this).attr("hour-block");
-    // use the eventID to get the hour
-    var formId = $(eventId);
-    console.log(formId);
-});
+// // function to push description to array upon save button click
+// $(".saveBtn").on("click", function () {
+//     // get the eventID of the button clicked
+//     var eventId = $(this).attr("hour-block");
+//     // use the eventID to get the hour
+//     var formId = $(eventId);
+//     console.log(formId);
+// });
