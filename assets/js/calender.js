0a7e895c7c56a932162012e56f26adf1eb53f1cc
AliMitchell <ali.mitchell.n@gmail.com>
2021-04-19 17:15:42 -0600

    display error

--- assets/js/calender.js
+++ assets/js/calender.js
@@ -88,7 +88,7 @@ function timeCodingWork(){
         if (moment().format("H") > 9 && moment().format("H") < 17) {
             $(`.work`).addClass("present");
 
-        } else if (moment().format("H") >= 17){
+        } else if (moment().format("H") >=17){
             $(`.work`).addClass("past");
         } else if (moment().format("H") < 9){
             $(`.work`).addClass("future");
@@ -98,61 +98,19 @@ function timeCodingWork(){
 timeCodingWork();  
 
 function timeCodingPM(){
-    //moment function to create time association for each hour
-       
-        if (moment().format("H") > 17 && moment().format("H") < 24) {
-            $(`.pm`).addClass("present");
+    //     if (moment().format("H") > 17 && moment().format("H") < 22) {
+    //         $(`.pm`).addClass("present");
+    //     }    
+    // };
+    if (moment().format("H") >= 17 && moment().format("H") < 20) {
+        $(`.pm`).addClass("present");
 
-        } else if (moment().format("H") >= 24){
-            $(`.pm`).addClass("past");
-        } 
-         
-    };
+    }
+};
 
 timeCodingPM();
 
-//change span to input on click to allow text 
-//on blur change back to span
-
-// $(".saveBtn").on("click", function(){
-//     var task = $(this)
-//         .siblings(".description")
-//         .val();
-
-//     console.log(task);
-//     //create another for parent element to grab id. pass function to save to local storage on click
-//     var save = $(this)
-//         .siblings(".description")
-//         .attr("id")
-//     console.log(save);
-
-//     localStorage.setItem(save, task);
-    
-// });
 
 
 
 
-
-
-
-
-// variable for events to be stored, pull from localStorage, set to empty array if falsy
-var events = JSON.parse(localStorage.getItem("events")) || [];
-
-// function to pull from localStorage
-var loadEvents = function() {
-    // set events object as pulled from localStorage
-    events = JSON.parse(localStorage.getItem("events"));
-    
-};
-
-
-// // function to push description to array upon save button click
-// $(".saveBtn").on("click", function () {
-//     // get the eventID of the button clicked
-//     var eventId = $(this).attr("hour-block");
-//     // use the eventID to get the hour
-//     var formId = $(eventId);
-//     console.log(formId);
-// });
